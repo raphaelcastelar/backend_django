a28c46231671be7530f12d746d651bab8c7009af
raphael castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-10-24 14:32:02 -0300

    a

--- backendgoytaborgs/backendgoytaborgs/settings.py
+++ backendgoytaborgs/backendgoytaborgs/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usuario'
 ]
 
 MIDDLEWARE = [
@@ -122,5 +121,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'usuario.CustomUser'
